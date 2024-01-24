1f6eafb82a0583427e4b2f0a56c2283b45553401
Ermis Ioannidis <docermis@gmail.com>
2024-01-24 15:53:17 +0200

    Added images and helper files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'Statistic-chart-generator-Admin-v2';
+  title = 'Statistic Chart Generator Admin V2';
 }
